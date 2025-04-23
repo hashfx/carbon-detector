0bf6eb4427e9d62922e015fc4abbbf5554a17b3d
Harsh Soni <algebra.soni@gmail.com>
2025-04-23 15:11:31 +0000

    add firebase auth keys

---
+++ carbon_counter/lib/firebase_options.dart
@@ -0,0 +1,13 @@
+// lib/firebase_options.dart
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+final FirebaseOptions defaultFirebaseOptions = FirebaseOptions(
+  apiKey: "AIzaSyC5zcXIlNQPWsW6Ps7HZCPnjcJrpXPDujY",
+  authDomain: "carbon-shodhak-122de.firebaseapp.com",
+  projectId: "carbon-shodhak-122de",
+  storageBucket: "carbon-shodhak-122de.firebasestorage.app",
+  messagingSenderId: "1020729262204",
+  appId: "1:1020729262204:web:202c86ca651691b48dc9f9"
+);
