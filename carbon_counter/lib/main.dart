0760b98e262b6541ea2f53b12c758e916658d85a
Harsh Soni <algebra.soni@gmail.com>
2025-04-23 15:12:00 +0000

    aff firebase auth

--- carbon_counter/lib/main.dart
+++ carbon_counter/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:carbon_counter/screens/auth_screen.dart';
+import 'package:carbon_counter/screens/carbon_data_screen.dart';
+import 'package:carbon_counter/firebase_options.dart';
 import 'package:carbon_counter/screens/splash_screen.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: defaultFirebaseOptions, // Use your manually created options
+  );
+
   tz.initializeTimeZones(); // Initialize timezone data once
   runApp(const MyApp());
 }
@@ -23,7 +34,13 @@ class MyApp extends StatelessWidget {
         // textTheme: TextTheme(...)
       ),
       debugShowCheckedModeBanner: false, // Remove debug banner
-      home: const SplashScreen(), // Start with the splash screen
+      home: const AuthScreen(), // Start with the Authentication screen
+      initialRoute: '/auth', // Start with the auth screen
+      routes: {
+        '/auth': (context) => const AuthScreen(),
+        '/carbon_data':
+            (context) => const CarbonDataScreen(), // Your main screen
+      },
     );
   }
 }
