68a051da1e9c6285ce3f4e43f1f369911f223812
Harsh Soni <algebra.soni@gmail.com>
2025-04-26 14:25:25 +0000

    modes of graph for settings page

---
+++ carbon_counter/lib/utils/graph_display_mode.dart
@@ -0,0 +1,6 @@
+// lib/utils/graph_display_mode.dart
+enum GraphDisplayMode {
+  none, // Display no graph
+  behindTable, // Display graph behind the table
+  standalone, // Display graph as a separate widget
+}
